6c6cd94f7d60672b798595791af0848125fe8d62
Bastien Collet <aso@foyer.lu>
2021-06-29 10:50:27 +0200

    Add app.component and item.service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit () {}
 }
---
+++ frontend/src/app/service/items.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {Observable, of} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemsService {
+
+  getItems(): Observable<string[]> {
+    return of(["jiptnkoucbrqgvmef", "aoh", "wahbtxmqpfzjkiecuy", "faxwbkzevhmctp", "favulrqbpshmicxwzje", "bgfkinpvwtrlomqujhyes", "wjluzheydimqxgvncrfb", "ctzsxgkfdinmuvew", "wtdmu", "vaz", "nojmrfvgiepdylsqh", "navqumpbcdeto", "qnbcg", "eznaijbvhp", "dgonkazqmxfihucrbselpwjtyv", "gzkwsnxqr", "qkebzvdwrgilatsmufhjxncypo", "idfyjotkhpnazgsvxequ", "qzajrtbfoviyldpmuwche", "hfpoagzbqrcmx", "cd", "woazvmpligbhksnfetcrdyu", "cnbytfolm", "slaebcyhfznmvpud", "qerimvzl", "honidaflr", "azgufxnsdi", "abnfcgmyhvdsikrxtquwzljo", "pslnaxgbryofhmeicvudqjzw", "hyadkipemcfqbvosrnugzj", "fzqa", "xpohwftrjdumesnbylckazg", "ldxaotivbmhryce", "kixyatpbjzoswuvh", "pt", "vlyafbkrmzotpdjgxhqcseniw", "lbyvtacm", "sdejforuvhlbazcinpqyktxw", "abcefvqrlxnhjptikusozg", "pfldagzwm", "ansmgojdvthrlzbpifqu", "fgebkdyhcuwtq", "ydcokgvxljptfbs", "uacolwbdiqfxpnhze", "ayuqfrbxelzwjvsgt", "hajgdbefokcisltmqzruxpyvnw", "lpwrgsqnfdmvxauctbihzjko", "jaxbwv", "hpndtvylk", "yzolmcsvhrpagufxdijkeqb", "fehaspdqovrzlnbcxuykigt", "pehoztngxiuqvardbswjk", "nakbispouczwxrdlemtfqjvgh", "hqmscgyrbztxpl", "bdetmpsyugroca", "mv", "pdnlfrvkchts", "pway", "acjrdozpshvxlw", "esdkumjpfciwhqvygoz", "oauntmbfwcqvzjhsydrkle", "pe", "pyevzlmkjragu", "gdbqawri", "ljdtercgfmphsxowiubkaqvz", "mlsjrabghzxfoipvc", "fnhuitravlmdq", "aptqfxobgryhcsidzlejmkn", "bcjhmwenl", "fklmgxon", "nbdsrxmlfjqhcuyotp", "tcbjvgfkm", "kopeumzhjicnabltdxrwfsvgqy", "haznldwmjtufvksxybgcqp", "bmwzrqtsadyfv", "fsqadwuoj", "rbdx", "wiavyhkxztdnjseofpm", "opsmaventruhqyfblkjicxz", "kvzaqlbtns", "gfbzshmkocxyejpq", "dcofbzaktlpsremgyhuqvni", "ivzumwgljkfndoacp", "fbiaogldhvjqksuxtcyewpz", "fuvmbaxjngirkzdtqhpcel", "eguftkrijsqxapbcvwmnydlho", "hbm", "zbpy", "nbskqvapftjlmduze", "gdnqcxeorsylupjbwatzvh", "defvlismk", "iylhskxntrzqvuajbcdeofgmw", "di", "gdushqbrickfmjzwnl", "djqptgfahmuszlbxwie", "iortwbgmlxauvkpfcnsjyzde", "aswbtgqpcxdeo", "panmbvj", "botysrwzpchijknfqu", "awiojcydzfneh"]);
+  }
+}
